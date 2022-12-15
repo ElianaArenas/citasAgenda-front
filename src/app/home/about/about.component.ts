501a6c1bd1466c61f9ee6bce4a3d557723d22371
ElianaArenas <elianaarenasm@gmail.com>
2022-12-15 00:25:39 -0500

    fix schedules

--- src/app/home/about/about.component.ts
+++ src/app/home/about/about.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { CompanyService } from '../../service/company.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
-export class AboutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class AboutComponent {
+  constructor(private companyService: CompanyService) {
+    this.getCompanyInformation();
   }
 
+  description: string = '';
+  title: string = '';
+
+  getCompanyInformation() {
+    this.companyService
+      .getCompany()
+      .subscribe(({ message: { descripcion, title } }) => {
+        this.description = descripcion;
+        this.title = title;
+      });
+  }
 }
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { CompanyI } from 'src/app/interfaces/company';
+import { CompanyService } from 'src/app/service/company.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
+  company!: CompanyI;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private companyService: CompanyService) {
+    this.getCompanyInformation();
   }
 
+  getCompanyInformation() {
+    this.companyService.getCompany().subscribe((company) => {
+      this.company = company;
+
+      console.log(this.company);
+    });
+  }
 }
