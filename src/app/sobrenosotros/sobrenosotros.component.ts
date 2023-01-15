63bbaa9d79917b6259ee0c8de0a82ff31aeaa567
ElianaArenas30 <120579832+ElianaArenas30@users.noreply.github.com>
2023-01-15 17:58:45 -0500

    styles

--- src/app/sobrenosotros/sobrenosotros.component.ts
+++ src/app/sobrenosotros/sobrenosotros.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faTwitter,
+  faFacebookF,
+  faLinkedinIn,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-sobrenosotros',
   templateUrl: './sobrenosotros.component.html',
-  styleUrls: ['./sobrenosotros.component.css']
+  styleUrls: ['./sobrenosotros.component.css'],
 })
-export class SobrenosotrosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class SobrenosotrosComponent {
+  faFacebook = faFacebookF;
+  faTwitter = faTwitter;
+  faLinkedinIn = faLinkedinIn;
+  constructor() {}
 }
