76dab8d52a60d8a6bbba37335181ac6da6d31420
ElianaArenas <elianaarenasm@gmail.com>
2023-04-10 23:52:08 -0500

    fix users

--- src/app/admin/register-user/register-user.component.ts
+++ src/app/admin/register-user/register-user.component.ts
@@ -9,32 +9,20 @@ import { AdminService } from 'src/app/service/admin.service';
   styleUrls: ['./register-user.component.css'],
 })
 export class RegisterUserComponent {
-  registerForm: FormGroup = this.fb.group(
-    {
-      nombre: ['', [Validators.required]],
-      genero: [''],
-      documento: ['', [Validators.required]],
-      email: ['', [Validators.required]],
-      codigo: ['', [Validators.required]],
-      idFamiliar: ['', [Validators.required]],
-      celular: [''],
-      categoria: [''],
-      direccion: ['', [Validators.required]],
-      barrio: ['', [Validators.required]],
-      newPassword: ['', [Validators.required /*, Validators.minLength(6)*/]],
-      confirmPassword: [
-        '',
-        [Validators.required /*, Validators.minLength(6)*/],
-      ],
-      rol: [''],
-      activo: [''],
-    },
-    {
-      validator: this.ConfirmedValidator('newPassword', 'confirmPassword'),
-    }
-  );
+  registerForm: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required]],
+    genero: [''],
+    documento: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    celular: [''],
+    categoria: [''],
+    direccion: ['', [Validators.required]],
+    barrio: ['', [Validators.required]],
+    rol: [''],
+    activo: [''],
+    color: [''],
+  });
 
-  color: any;
   generos: string[] = ['F', 'M', 'Otro'];
   userStatus: string[] = ['Inactivo', 'Activar', 'Desactivar'];
   roles: string[] = ['Administrador', 'Profesor', 'Canchero', 'Socio'];
@@ -52,33 +40,31 @@ export class RegisterUserComponent {
       genero,
       documento,
       email,
-      codigo,
-      idFamiliar,
       celular,
       categoria,
       direccion,
       barrio,
       torneos,
       newPassword,
-      confirmPassword,
       rol,
       activo,
+      color,
     } = this.registerForm.value;
 
     const createUser = {
       nombre: nombre,
-      codigo: codigo,
+      codigo: documento,
       documento: documento,
       celular: celular,
       direccion: direccion,
       activo: activo === 'Activar',
-      grupoFamiliar: idFamiliar,
+      grupoFamiliar: 1,
       rol,
-      contra: newPassword,
+      contra: documento,
       email,
       genero: genero,
       barrio: barrio,
-      color: this.color,
+      color,
     };
 
     this.adminService.createUser(createUser).subscribe((res) => {
@@ -87,6 +73,18 @@ export class RegisterUserComponent {
         return;
       }
       Swal.fire('Excelente', 'Usuario creado exitosamente', 'success');
+      this.registerForm.get('nombre')?.setValue('');
+      this.registerForm.get('genero')?.setValue('');
+      this.registerForm.get('documento')?.setValue('');
+      this.registerForm.get('email')?.setValue('');
+      this.registerForm.get('celular')?.setValue('');
+      this.registerForm.get('categoria')?.setValue('');
+      this.registerForm.get('direccion')?.setValue('');
+      this.registerForm.get('barrio')?.setValue('');
+      this.registerForm.get('rol')?.setValue('');
+      this.registerForm.get('activo')?.setValue('');
+
+      this.registerForm.markAsUntouched();
     });
   }
 
